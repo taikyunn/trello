f4d60529312dfb02a54e9d7de4cdf4cd4392b545
taikyunn <taichi_141@me.com>
2020-09-24 19:29:01 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  has_many :lists
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, length: { maximum: 10, message: 'は10文字以下で入力してください' }
+         with_options presence: true do
+          validates :nickname, length: { maximum: 10, message: 'は10文字以下で入力してください' }
+         end
+
 end
