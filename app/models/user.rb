aa722d4e48cecc203fb0a94ebb9795f57d13a94c
taikyunn <taichi_141@me.com>
2020-09-24 15:44:13 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true, length: { maximum: 10, message: 'は10文字以下で入力してください'}
+  validates :nickname, presence: true, length: { maximum: 10, message: 'は10文字以下で入力してください' }
 end
