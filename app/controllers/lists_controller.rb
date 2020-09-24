f5c71a85c4b3abf8baffa4e51b6140f488624b4b
taikyunn <taichi_141@me.com>
2020-09-24 18:40:25 +0900

    destroyアクション

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_params, only: [:edit,:update]
+  before_action :set_params, only: [:edit,:update,:destroy]
 
   def new
     @list = List.new
@@ -21,6 +21,11 @@ class ListsController < ApplicationController
     else
       ender action: :edit
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to root_path
   end
 
   private
