b76c146d51dde7d5b217024e575a03f417e71f82
taikyunn <taichi_141@me.com>
2020-09-25 15:47:04 +0900

    うまく行く時完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe User do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    describe 'ユーザー新規登録' do
+      context '新規登録がうまく行く時' do
+        it '全てがうまくいけば登録できる' do
+          expect(@user).to be_valid
+        end
+      end
+      context '新規登録がうまくいかない時' do
+        
+      end
+    end
+  end
 end
