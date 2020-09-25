b31550db00fdff0d8c40aed1d5dc1a373c4f433e
taikyunn <taichi_141@me.com>
2020-09-25 20:19:53 +0900

    view完了

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,2 +1,25 @@
 class CardController < ApplicationController
+
+  def new
+    @card = Card.new
+    @list = List.find_by(params[:list_id])
+  end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:title,:memo,:list_id)
+  end
+
+  def set_params
+    @card = Card.find(params[:id])
+  end
 end
