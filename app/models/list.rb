05771b64c86668e4f7f8a314a0af9d91251326eb
taikyunn <taichi_141@me.com>
2020-09-29 11:19:58 +0900

    listモデルバリテーション

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
   has_many :cards, dependent: :destroy
+
+  validates :title, presence: true
 end
--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :card do
-    title {'タイトル'}
-    memo {'メモ'}
+    
   end
 end
