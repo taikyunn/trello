5efd1e6e326f1324c807560b34d9833031be61c6
taikyunn <taichi_141@me.com>
2020-09-29 11:38:46 +0900

    日本語化完了

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -14,7 +14,17 @@ RSpec.describe List, type: :model do
         end
       end
       context 'リスト作成がうまくいかない時' do
-        
+        it 'titleが空だと登録できない' do
+          @list.title = ''
+          @list.valid?
+          expect(@list.errors.full_messages).to include("タイトルを入力してください")
+        end
+        it 'titileが10文字より多いと登録できない' do
+          @list.title = 'タイトルタイトルタイトル'
+          @list.valid?
+          expect(@list.errors.full_messages).to include("タイトルは10文字以下で入力してください")
+        end
+
       end
     end
   end
