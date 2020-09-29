26fd6ce2be4508c5a1fdcab3b91757cae77eeadc
taikyunn <taichi_141@me.com>
2020-09-29 12:39:51 +0900

    コントローラー編集

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,29 +1,18 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
   def index
     @events = Event.all
     @list = List.where(params[:list_id]).first
   end
 
-  # GET /events/1
-  # GET /events/1.json
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
-  def edit
-  end
-
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
 
@@ -38,8 +27,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -52,8 +39,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
@@ -63,12 +48,10 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def event_params
       params.require(:event).permit(:title, :description, :start_date, :end_date)
     end
