4122ff4f1a8720ae219d4466dad278454876ab20
taikyunn <taichi_141@me.com>
2020-09-30 17:54:23 +0900

    マイページ遷移完了

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -34,7 +34,7 @@ class CardController < ApplicationController
   private
 
   def card_params
-    params.require(:card).permit(:title, :memo, :list_id)
+    params.require(:card).permit(:title, :memo, :list_id, :start_date, :end_date)
   end
 
   def set_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @nickname = current_user.nickname
+    @lists = current_user.lists
+
+
+  end
+
 end
