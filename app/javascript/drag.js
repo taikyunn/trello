16fe2b6f2e7b8966e945169af7a9f700399cd118
taikyunn <taichi_141@me.com>
2020-10-01 16:09:22 +0900

    コミット

---
+++ app/javascript/drag.js
@@ -0,0 +1,27 @@
+function drag(){
+  const card = document.getElementById("cardDrag");
+
+      card.onmousedown = function(event){
+        document.addEventListener("mousemove",onMouseMove);
+
+      const onMouseMove = function(event){
+          const x = event.clientX;
+          const y = event.clientY;
+          const width = card.offsetWidth;
+          const height = card.offsetHeight;
+          card.style.top = (y-height/2) + "px";
+          card.style.left = (x-width/2) + "px";
+        }
+      card.onmouseup = function(event){
+          const x = event.clientX;
+          const y = event.clientY;
+          const width = card.offsetWidth;
+          const height = card.offsetHeight;
+          const list = document.getElementById("listDrag");
+          if((x>=list.left && x<=(list.left+list.width)) && (y>=list.top && y<=(list.top+list.height)))
+
+          list.appendChild(card)
+      }
+  }
+}
+window.addEventListener("load", drag); 
