5e290002a46a1a4bff414c0aa6e9330487498804
taikyunn <taichi_141@me.com>
2020-10-01 16:09:01 +0900

    コミット

--- app/javascript/drag.js
+++ app/javascript/drag.js
@@ -1,27 +1,31 @@
 function drag(){
-    const card = document.getElementById("cardDrag");
-        card.onmousedown = function(event){
-            document.addEventListener("mousemove",onMouseMove);
-            card.onmousedown = function(event){
-                console.log("タップされたよ!");
-              }
+  const card = document.getElementById("cardDrag");
+
+      card.onmousedown = function(event){
+        document.addEventListener("mousemove",onMouseMove);
+
+      const onMouseMove = function(event){
+          const x = event.clientX;
+          const y = event.clientY;
+          const width = card.offsetWidth;
+          const height = card.offsetHeight;
+          card.style.top = (y-height/2) + "px";
+          card.style.left = (x-width/2) + "px";
         }
-        const onMouseMove = function(event){
-            const x = event.clientX;
-            const y = event.clientY;
-            const width = card.offsetWidth;
-            const height = card.offsetHeight;
-            card.style.top = (y-height/2) + "px";
-            card.style.left = (x-width/2) + "px";
-          }
-        card.onmouseup = function(event){
-            const x = event.clientX;
-            const y = event.clientY;
-            const width = card.offsetWidth;
-            const height = card.offsetHeight;
-              const list = document.getElementById("listDrag");
-    
-            }
-          }
-    window.addEventListener("load", drag); 
-    
+      card.onmouseup = function(event){
+          const x = event.clientX;
+          const y = event.clientY;
+          const width = card.offsetWidth;
+          const height = card.offsetHeight;
+          const list = document.getElementById("listDrag");
+          if((x>=list.left && x<=(list.left+list.width)) && (y>=list.top && y<=(list.top+list.height)))
+
+          list.appendChild(card)
+      }
+  }
+}
+window.addEventListener("load", drag); 
+
+
+
+
