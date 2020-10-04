88f8d290e32702d2aecc0ed113887429bc9c877c
taikyunn <taichi_141@me.com>
2020-10-05 08:06:18 +0900

    コミット

--- app/javascript/drag.js
+++ app/javascript/drag.js
@@ -1,31 +1,22 @@
-function drag(){
-  const card = document.getElementById("cardDrag");
-
-      card.onmousedown = function(event){
-        document.addEventListener("mousemove",onMouseMove);
-
-      const onMouseMove = function(event){
-          const x = event.clientX;
-          const y = event.clientY;
-          const width = card.offsetWidth;
-          const height = card.offsetHeight;
-          card.style.top = (y-height/2) + "px";
-          card.style.left = (x-width/2) + "px";
-        }
-      card.onmouseup = function(event){
-          const x = event.clientX;
-          const y = event.clientY;
-          const width = card.offsetWidth;
-          const height = card.offsetHeight;
-          const list = document.getElementById("listDrag");
-          if((x>=list.left && x<=(list.left+list.width)) && (y>=list.top && y<=(list.top+list.height)))
-
-          list.appendChild(card)
-      }
-  }
+function dragstart(event){
+  //ドラッグするデータのid名をDataTransferオブジェクトにセット
+  event.dataTransfer.setData("text", event.target.id);
 }
-window.addEventListener("load", drag); 
+/***** ドラッグ要素がドロップ要素に重なっている間の処理 *****/
+function dragover(event){
+  //dragoverイベントをキャンセルして、ドロップ先の要素がドロップを受け付けるようにする
+  event.preventDefault();
+}
 
-
-
-
+/***** ドロップ時の処理 *****/
+function drop(event){
+  //ドラッグされたデータのid名をDataTransferオブジェクトから取得
+  const id_name = event.dataTransfer.getData("text");
+  console.log(id_name)
+  //id名からドラッグされた要素を取得
+  const drag_elm =document.getElementById(id_name);
+  //ドロップ先にドラッグされた要素を追加
+  event.currentTarget.appendChild(drag_elm);
+  //エラー回避のため、ドロップ処理の最後にdropイベントをキャンセルしておく
+  event.preventDefault();
+}
