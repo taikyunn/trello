ce8c79716edafaaa72cac741a0405f5218bb5bb9
taikyunn <taichi_141@me.com>
2020-10-06 15:09:46 +0900

    バリテーション 完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_many :lists
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
+         has_many :sns_credentials
+         
   with_options presence: true do
     validates :nickname, length: { maximum: 10, message: 'は10文字以下で入力してください' }
     validates :email, uniqueness: true
