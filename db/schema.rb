fd8f98db54e8e4007de85c73aaf62be827a7a02f
taikyunn <taichi_141@me.com>
2020-10-06 15:07:53 +0900

    マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_014908) do
+ActiveRecord::Schema.define(version: 2020_10_06_060706) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2020_10_05_014908) do
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,4 +72,5 @@ ActiveRecord::Schema.define(version: 2020_10_05_014908) do
   end
 
   add_foreign_key "cards", "lists"
+  add_foreign_key "sns_credentials", "users"
 end
