183aadd6814057cbcd4b91cfb04128093a96d5d9
taikyunn <taichi_141@me.com>
2020-10-13 14:48:25 +0900

    jquery導入設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,7 @@ require("channels")
 require("../calendar")
 require("../drag")
 require("../star")
-
-// require("../checked")
-//= require jquery.raty.js
-
+require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
