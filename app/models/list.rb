a07ec63384d8162f1c74f154d38a4a418a1b3803
taikyunn <taichi_141@me.com>
2020-11-05 16:55:21 +0900

    listコントローラーバリテーション 完了

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,8 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :cards, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 10, message: 'は10文字以下で入力してください' }
+  with_options presence: true do
+    validates :title, length: { maximum: 10, message: 'は10文字以下で入力してください' }
+    validates :description
+  end
 end
