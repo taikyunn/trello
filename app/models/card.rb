bae7da7930235357a6d2723cdd397f9e0ea5b09a
taikyunn <taichi_141@me.com>
2020-11-07 11:50:53 +0900

    cardコントローラーテストコード

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
   belongs_to :list
 
   with_options presence: true do
-    validates :title, length: { in: 1..255, message: 'は255文字以下で入力してください' }
-    validates :memo, length: { maximum: 1000, message: 'は1000文字以下で入力してください' }
+    validates :title, length: { in: 1..255, message: 'は1~255文字以下で入力してください' }
+    validates :memo, length: { in: 1..255, message: 'は1~1000文字以下で入力してください' }
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe List do
+    before do
+      @user= FactoryBot.create(:user)
+      @list = FactoryBot.create(:list, user_id: @user.id)
+      @card = FactoryBot.build(:card, list_id: @list.id)
+    end
+
+    describe 'カード作成' do
+      context 'カード作成がうまくいく時' do
+        it '全てがうまくいけば作成できる' do
+          expect(@card).to be_valid
+        end
+      end
+
+      context 'カード作成がうまくいかない時' do
+        it 'titleが1~255文字でないと作成できない' do
+          @card.title = ''
+          @card.valid?
+          expect(@card.errors.full_messages).to include('Titleは1~255文字以下で入力してください')
+        end
+        it 'memoが1~1000文字出ないと作成できない' do
+          @card.memo = ''
+          @card.valid?
+          expect(@card.errors.full_messages).to include('Memoは1~1000文字以下で入力してください')
+        end
+      end
+    end
+  end
 end
