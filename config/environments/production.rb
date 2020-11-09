ee5b7a22b306091512b5e947a7e5c0058bb298ef
taikyunn <taichi_141@me.com>
2020-11-09 17:19:18 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,10 +114,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name      => 'app189367167@heroku.com',
+    :password       => '3prtixwp5166',
     :domain => "herokuapp.com",
-    :address => "smtp.gmail.com",
+    :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
