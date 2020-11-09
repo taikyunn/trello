ef94aba003d35f95186e0c0f29181da85b9c3f4e
taikyunn <taichi_141@me.com>
2020-11-09 15:12:23 +0900

    確認

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,9 +110,11 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = {  host: 'https://trellotest-taichi.herokuapp.com', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  host = 'https://trellotest-taichi.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   # mail setting
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:  "smtp.gmail.com",
     enable_starttls_auto: true,
