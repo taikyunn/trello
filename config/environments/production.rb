5fb494295fe9c3a0381af0a251a3dd36a67c3156
taikyunn <taichi_141@me.com>
2020-11-10 10:14:28 +0900

    変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,10 +113,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.gmail.com',
-    port:            '587',
+    port:            587,
     user_name:       ENV['GMAIL_USERNAME'],
     password:        ENV['GMAIL_PASSWORD'],
     domain:          'gmail.com',
-    authentication: :login
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
