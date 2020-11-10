8d30c46fd17150e8e661ccaaa5023689a630cfb7
taikyunn <taichi_141@me.com>
2020-11-10 12:09:28 +0900

    fullcalender　修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,9 @@ require("../drag")
 //= require popper
 //= require bootstrap-sprockets
 require('jquery')
+//= require jquery
+//= require moment
+//= require fullcalendar
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
