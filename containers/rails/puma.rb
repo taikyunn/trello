641326e98c4543586a54918df84909d81b4b2220
taikyunn <taichi_141@me.com>
2020-11-13 21:15:04 +0900

    dockerfile 編集

--- containers/rails/puma.rb
+++ containers/rails/puma.rb
@@ -5,5 +5,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+bind "unix:// app/tmp/sockets/puma.sock"
 
