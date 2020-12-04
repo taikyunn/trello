dd6de7a5fbc03197951ea2ff461d4b7f24e850a4
taikyunn <taichi_141@me.com>
2020-12-04 15:08:42 +0900

    修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,48 +1,7 @@
 class EventsController < ApplicationController
-  before_action :set_list, only: [:edit, :update, :destroy, :show]
 
-  def index
-    @lists = List.all
-    @list = List.where(params[:list_id]).first
-  end
-
-  def new
-    @event = Event.new
-  end
-
-  def create
-    @list = List.new(list_param)
-    if @list.save
-      render action: :index
-    else
-      render action: :new
-    end
-  end
-
-  def update
-    if @list.update(list_params)
-      redirect_to root_path
-    else
-      render action: :edit
-    end
-  end
-
-  def destroy
-    @list.destroy
-    redirect_to action: :index
-  end
-
-  private
-
-  def set_list
+  def show
     @list = List.find(params[:id])
   end
-
-  def list_param
-    params.permit(:title, :description, :start_date, :end_date).merge(user_id: current_user.id)
-  end
-
-  def list_params
-    params.require(:list).permit(:title, :description, :start_date, :end_date).merge(user_id: current_user.id)
-  end
+  
 end
